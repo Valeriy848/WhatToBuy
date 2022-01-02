1c6566345e81b07333f7b84aff78cb7962e70ea6
ValeriyPokatilo <valeriy.pokatilo@yandex.ru>
2022-01-02 15:30:37 +0700

    Создание и настройка проекта
    [WTB-1]

---
+++ WhatToBuy/AppFiles/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  WhatToBuy
+//
+//  Created by Valeriy Pokatilo on 02.01.2022.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+    }
+}
