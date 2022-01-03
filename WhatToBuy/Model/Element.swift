a84a090f768a1d9e3384a45b15bdebc8ce08fa8a
ValeriyPokatilo <valeriy.pokatilo@yandex.ru>
2022-01-03 18:36:20 +0700

    Модель для элемента
    [WTB-4]

---
+++ WhatToBuy/Model/Element.swift
@@ -0,0 +1,13 @@
+//
+//  Element.swift
+//  WhatToBuy
+//
+//  Created by Valeriy Pokatilo on 03.01.2022.
+//
+
+import Foundation
+
+internal struct Element{
+    let title: String
+    let date: Date
+}
